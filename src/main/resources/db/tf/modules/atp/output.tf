6e16877b38a4f8fc55ed473bb5da6b3d8d80e09f
cpruvost <christophe.pruvost@oracle.com>
2022-09-19 16:57:26 +0200

    atp url

--- src/main/resources/db/tf/modules/atp/output.tf
+++ src/main/resources/db/tf/modules/atp/output.tf
@@ -8,11 +8,11 @@ data "oci_database_autonomous_databases" "autonomous_databases" {
   
 }
 
-output "autonomous_database_high_connection_string" {
+#output "autonomous_database_high_connection_string" {
   #value = "${lookup(oci_database_autonomous_database.tf_adb.connection_strings.0.all_connection_strings, "high", "unavailable")}"
   #value = "${lookup(oci_database_autonomous_database.tf_adb.connection_strings.0.all_connection_strings, "high", "unavailable")}"
   #value = "${data.oci_database_autonomous_databases.autonomous_databases.autonomous_databases.connection_strings.0.high}"
-}
+#}
 
 output "autonomous_databases" {
   value = "${data.oci_database_autonomous_databases.autonomous_databases.autonomous_databases}"
